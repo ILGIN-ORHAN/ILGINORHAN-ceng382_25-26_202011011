1cbb8ea16484337c0fd50e365c33efdbea7141f0
ILGIN-ORHAN <ilginorhan499@gmail.com>
2025-04-29 10:33:10 +0300

    delete and edit buttons are added

--- Week5Project/Pages/Index.cshtml.cs
+++ Week5Project/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-// Pages/Index.cshtml.cs
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
@@ -9,72 +8,73 @@ using System.ComponentModel.DataAnnotations;
 namespace Week5Project.Pages
 {
     public class IndexModel : PageModel
+    {
+        public static List<ClassInformationModel> ClassList { get; set; } = new();
+        private static int _nextId = 1;
+
+        [BindProperty]
+        public ClassInformationModel NewClass { get; set; } = new();
+
+        [BindProperty]
+        public bool IsEditing { get; set; }
+
+        public void OnGet()
         {
-            public static List<ClassInformationModel> ClassList { get; set; } = new();
-            private static int _nextId = 1;
-
-            [BindProperty]
-            public ClassInformationModel NewClass { get; set; }
-
-            [BindProperty]
-            public bool IsEditing { get; set; }
-
-            public void OnGet()
-            {
-            }
-
-            public IActionResult OnPostAdd()
-            {
-                if (!ModelState.IsValid)
-                    return Page();
-
-                NewClass.Id = _nextId++;
-                ClassList.Add(NewClass);
-                return RedirectToPage();
-            }
-
-            public IActionResult OnPostDelete(int id)
-            {
-                var item = ClassList.FirstOrDefault(c => c.Id == id);
-                if (item != null)
-                    ClassList.Remove(item);
-
-                return RedirectToPage();
-            }
-
-            public IActionResult OnPostSelectForEdit(int id)
-            {
-                var classToEdit = ClassList.FirstOrDefault(c => c.Id == id);
-                if (classToEdit != null)
-                {
-                    NewClass = new ClassInformationModel
-                    {
-                        Id = classToEdit.Id,
-                        ClassName = classToEdit.ClassName,
-                        StudentCount = classToEdit.StudentCount,
-                        Description = classToEdit.Description
-                    };
-                    IsEditing = true;
-                }
-
-                return Page();
-            }
-
-            public IActionResult OnPostUpdate()
-            {
-                if (!ModelState.IsValid)
-                    return Page();
-
-                var existing = ClassList.FirstOrDefault(c => c.Id == NewClass.Id);
-                if (existing != null)
-                {
-                    existing.ClassName = NewClass.ClassName;
-                    existing.StudentCount = NewClass.StudentCount;
-                    existing.Description = NewClass.Description;
-                }
-
-                return RedirectToPage();
-            }
         }
 
+        public IActionResult OnPostAdd()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            NewClass.Id = _nextId++;
+            ClassList.Add(NewClass);
+            NewClass = new ClassInformationModel();
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            var item = ClassList.FirstOrDefault(c => c.Id == id);
+            if (item != null)
+                ClassList.Remove(item);
+
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostSelectForEdit(int id)
+        {
+            var classToEdit = ClassList.FirstOrDefault(c => c.Id == id);
+            if (classToEdit != null)
+            {
+                NewClass = new ClassInformationModel
+                {
+                    Id = classToEdit.Id,
+                    ClassName = classToEdit.ClassName,
+                    StudentCount = classToEdit.StudentCount,
+                    Description = classToEdit.Description
+                };
+                IsEditing = true;
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostUpdate()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var existing = ClassList.FirstOrDefault(c => c.Id == NewClass.Id);
+            if (existing != null)
+            {
+                existing.ClassName = NewClass.ClassName;
+                existing.StudentCount = NewClass.StudentCount;
+                existing.Description = NewClass.Description;
+            }
+
+            IsEditing = false;
+            NewClass = new ClassInformationModel();
+            return RedirectToPage();
+        }
+    }
 }
